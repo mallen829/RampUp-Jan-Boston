ec11db37e3dfe6abd2cd905526a298ca918bf185
Melissa Allen <melissa.allen14@gmail.com>
2015-01-19 15:33:26 -0500

    added conversion formula

--- fahrenheit_to_celcius.rb
+++ fahrenheit_to_celcius.rb
@@ -1,6 +1,9 @@
 #convert fahrenheit to celcius
 def fahrenheit_to_celcius
-	puts "butts"
+	puts "What is the tempurature?\n"
+	fahrenheit = gets.chomp.to_f
+	celcius = (fahrenheit - 32.0) * (5.0/9.0)
+	puts celcius
 end
 
 fahrenheit_to_celcius
