ee9d28f9157cb6d1d6e6015a1d9ee0e36322a9e4
Melissa Allen <melissa.allen14@gmail.com>
2015-01-19 15:37:12 -0500

    created question and answer text

--- fahrenheit_to_celcius.rb
+++ fahrenheit_to_celcius.rb
@@ -1,9 +1,9 @@
 #convert fahrenheit to celcius
 def fahrenheit_to_celcius
-	puts "What is the tempurature?\n"
+	puts "What is the tempurature in Fahrenheit?\n"
 	fahrenheit = gets.chomp.to_f
 	celcius = (fahrenheit - 32.0) * (5.0/9.0)
-	puts celcius
+	puts "The tempurature in Celsius is #{celcius} degrees."
 end
 
 fahrenheit_to_celcius
